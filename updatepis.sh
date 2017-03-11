f2a1248019a35351a531e8b5d4575da9dc063232
Jon Hearn <jhearn@ft-services.com>
2017-03-11 07:31:53 +0000

    update pis

---
+++ updatepis.sh
@@ -0,0 +1,12 @@
+echo 101
+sshpass -p 'raspberry' ssh pi@192.168.0.101 "cd dojo_pygame && ./update.sh"
+echo 102
+sshpass -p 'raspberry' ssh pi@192.168.0.102 "cd dojo_pygame && ./update.sh"
+echo 103
+sshpass -p 'raspberry' ssh pi@192.168.0.103 "cd dojo_pygame && ./update.sh"
+echo 104
+sshpass -p 'raspberry' ssh pi@192.168.0.104 "cd dojo_pygame && ./update.sh"
+echo 105
+sshpass -p 'raspberry' ssh pi@192.168.0.105 "cd dojo_pygame && ./update.sh"
+echo 110
+sshpass -p 'raspberry' ssh pi@192.168.0.110 "cd dojo_pygame && ./update.sh"
